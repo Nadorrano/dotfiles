676da5821af70d341844e87bf186fa38b5b106fa
Nadorrano <fonta.spezia@gmail.com>
2015-10-15 10:23:43 +0000

    Removed unused /bin. Added git-all as an alias to git aliases

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,11 +1,3 @@
-# Use `hub` as our git wrapper:
-#   http://defunkt.github.com/hub/
-hub_path=$(which hub)
-if (( $+commands[hub] ))
-then
-  alias git=$hub_path
-fi
-
 # The rest of my fun git aliases
 alias gl='git pull --prune'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
@@ -17,3 +9,4 @@ alias gco='git checkout'
 alias gcb='git copy-branch-name'
 alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
+alias git-all='git add -A'
