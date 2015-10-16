7c22c0681bf4ff86ab9f5d8e30dd233998bdb73d
Nadorrano <fonta.spezia@gmail.com>
2015-10-16 14:59:41 +0000

    Fixing zsh aliases.

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -1,5 +1,5 @@
 #
-# Store here all aliases that not depend on the console used or 
+# Store here all aliases that not depend on the shell used or 
 # does not belong to any topic folder.
 #
 
@@ -8,14 +8,14 @@ alias ls='ls --color=auto'
 alias la='ls -a' 
 alias ll='ls -la'
 
-# sudo alias, execute last command again, using sudo
+# execute last command again, using sudo
 alias fuck='sudo $(fc -nl -1)'
 
 # colored grep
 alias grep='grep --color'
 
 # Bumblebee
-# Use gpu for rendering (if optirun is not working)
+# Use gpu for rendering (if optirun is not working) for nvidia hardware
 # from user flecha https://bbs.archlinux.org/viewtopic.php?id=184571 
 function use_gpu { xrandr --setprovideroffloadsink nvidia Intel;  DRI_PRIME=1 $@; }
 
---
+++ zsh/keybinds.zsh
@@ -0,0 +1,27 @@
+#
+# key mapping for zsh
+#
+
+# Back to previous folder: Alt + LeftArrow
+# Up to parent folder: Alt + UpArrow
+cdUndoKey() {
+  popd      > /dev/null
+  zle       reset-prompt
+  echo
+  ls
+  echo
+}
+
+cdParentKey() {
+  pushd .. > /dev/null
+  zle      reset-prompt
+  echo
+  ls
+  echo
+}
+
+zle -N                 cdParentKey
+zle -N                 cdUndoKey
+bindkey '^[[1;3A'      cdParentKey
+bindkey '^[[1;3D'      cdUndoKey
+
