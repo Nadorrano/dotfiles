eab127212e21730873830b0a0945c05fd9f6fd40
Nadorrano <fonta.spezia@gmail.com>
2015-11-04 13:20:57 +0100

    new prompt

--- zsh/keybinds.zsh
+++ zsh/keybinds.zsh
@@ -2,42 +2,22 @@
 # key mapping for zsh
 #
 
-typeset -g -A key
 
-key[F1]=''
-key[F2]='^[OQ'
-key[F3]='^[OR'
-key[F4]='^[OS'
-key[F5]='^[[15~'
-key[F6]='^[[17~'
-key[F7]='^[[18~'
-key[F8]='^[[19~'
-key[F9]='^[[20~'
-key[F10]='^[[21~'
-key[F11]=''
-key[F12]='^[[24~'
-key[Backspace]='^?'
-key[Insert]='^[[2~'
-key[Home]='^[OH'
-key[PageUp]='^[[5~'
-key[Delete]='^[[3~'
-key[End]='^[OF'
-key[PageDown]='^[[6~'
-key[Up]='^[[A'
-key[Left]='^[[D'
-key[Down]='^[[B'
-key[Right]='^[[C'
-key[Menu]=''
+# Use emacs-style keybindings
+bindkey -e
 
-#  actually bind the keys
-bindkey "${key[Home]}"          beginning-of-line
-bindkey "${key[End]}"           end-of-line
-bindkey "${key[Insert]}"        overwrite-mdoe
-bindkey "${key[Delete]}"        delete-char
-bindkey "${key[Up]}"            history-search-backward #up-line-or-history
-bindkey "${key[Down]}"          history-search-forward # down-line-or-history
-bindkey "${key[Left]}"          backward-char
-bindkey "${key[Right]}"         forward-char
+bindkey "$terminfo[khome]" beginning-of-line # Home
+bindkey "$terminfo[kend]" end-of-line # End
+bindkey "$terminfo[kich1]" overwrite-mode # Insert
+bindkey "$terminfo[kdch1]" delete-char # Delete
+bindkey "$terminfo[kcuu1]" up-line-or-history # Up
+bindkey "$terminfo[kcud1]" down-line-or-history # Down
+bindkey "$terminfo[kcub1]" backward-char # Left
+bindkey "$terminfo[kcuf1]" forward-char # Right
+
+# Bind ctrl-left / ctrl-right
+bindkey "\e[1;5D" backward-word
+bindkey "\e[1;5C" forward-word"]]"
 
 # Back to previous folder: Alt + LeftArrow
 # Up to parent folder: Alt + UpArrow
--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,82 +1,31 @@
-autoload colors && colors
-# cheers, @ehrenmurdick
-# http://github.com/ehrenmurdick/config/blob/master/zsh/prompt.zsh
+# Let's have some colors first
+autoload -U colors && colors
 
-if (( $+commands[git] ))
-then
-  git="$commands[git]"
+if [[ -e /usr/share/zsh/site-contrib/powerline.zsh ]]; then
+  # Powerline support is enabled if available, otherwise use a regular PS1
+  . /usr/share/zsh/site-contrib/powerline.zsh
+  VIRTUAL_ENV_DISABLE_PROMPT=true
 else
-  git="/usr/bin/git"
+  # Default colors:
+  # Cyan for users, red for root, magenta for system users
+  local _time="%{$fg[yellow]%}[%*]"
+  local _path="%B%{$fg[green]%}%(8~|...|)%7~"
+  local _usercol
+  if [[ $EUID -lt 1000 ]]; then
+    # red for root, magenta for system users
+    _usercol="%(!.%{$fg[red]%}.%{$fg[magenta]%})"
+  else
+    _usercol="$fg[cyan]"
+  fi
+  local _user="%{$_usercol%}%n@%M"
+  local _prompt="%{$fg[white]%}${(r:$SHLVL*2::%#:)}"
+  
+  PROMPT=$'$_time $_user $_path $_prompt%b%f%k ${vcs_info_msg_0_}\n› '
+
+  # RPROMPT='${vcs_info_msg_0_}' # git branch
+  if [[ ! -z "$SSH_CLIENT" ]]; then
+    RPROMPT="$RPROMPT ⇄" # ssh icon
+  fi
 fi
 
-git_branch() {
-  echo $($git symbolic-ref HEAD 2>/dev/null | awk -F/ {'print $NF'})
-}
 
-git_dirty() {
-  if $(! $git status -s &> /dev/null)
-  then
-    echo ""
-  else
-    if [[ $($git status --porcelain) == "" ]]
-    then
-      echo "on %{$fg_bold[green]%}$(git_prompt_info)%{$reset_color%}"
-    else
-      echo "on %{$fg_bold[red]%}$(git_prompt_info)%{$reset_color%}"
-    fi
-  fi
-}
-
-git_prompt_info () {
- ref=$($git symbolic-ref HEAD 2>/dev/null) || return
-# echo "(%{\e[0;33m%}${ref#refs/heads/}%{\e[0m%})"
- echo "${ref#refs/heads/}"
-}
-
-unpushed () {
-  $git cherry -v @{upstream} 2>/dev/null
-}
-
-need_push () {
-  if [[ $(unpushed) == "" ]]
-  then
-    echo " "
-  else
-    echo " with %{$fg_bold[magenta]%}unpushed%{$reset_color%} "
-  fi
-}
-
-ruby_version() {
-  if (( $+commands[rbenv] ))
-  then
-    echo "$(rbenv version | awk '{print $1}')"
-  fi
-
-  if (( $+commands[rvm-prompt] ))
-  then
-    echo "$(rvm-prompt | awk '{print $1}')"
-  fi
-}
-
-rb_prompt() {
-  if ! [[ -z "$(ruby_version)" ]]
-  then
-    echo "%{$fg_bold[yellow]%}$(ruby_version)%{$reset_color%} "
-  else
-    echo ""
-  fi
-}
-
-directory_name() {
-  echo "%{$fg_bold[cyan]%}%1/%\/%{$reset_color%}"
-}
-
-export PROMPT=$'\n$(rb_prompt)in $(directory_name) $(git_dirty)$(need_push)\n› '
-set_prompt () {
-  export RPROMPT="%{$fg_bold[cyan]%}%{$reset_color%}"
-}
-
-precmd() {
-  title "zsh" "%m" "%55<...<%~"
-  set_prompt
-}
