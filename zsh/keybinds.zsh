9d898dcb9ea24a2f5999367da62069604a1a4d98
Nadorrano <fonta.spezia@gmail.com>
2017-09-07 18:17:53 +0200

    Fix keybindings

--- zsh/keybinds.zsh
+++ zsh/keybinds.zsh
@@ -2,22 +2,52 @@
 # key mapping for zsh
 #
 
-
-# Use emacs-style keybindings
+# emacs-like keybindings
 bindkey -e
 
-bindkey "$terminfo[khome]" beginning-of-line # Home
-bindkey "$terminfo[kend]" end-of-line # End
-bindkey "$terminfo[kich1]" overwrite-mode # Insert
-bindkey "$terminfo[kdch1]" delete-char # Delete
-bindkey "$terminfo[kcuu1]" up-line-or-history # Up
-bindkey "$terminfo[kcud1]" down-line-or-history # Down
-bindkey "$terminfo[kcub1]" backward-char # Left
-bindkey "$terminfo[kcuf1]" forward-char # Right
+# Read terminfo
+typeset -A key
+
+key[Home]="$terminfo[khome]"
+key[End]="$terminfo[kend]"
+key[Insert]="$terminfo[kich1]"
+key[Backspace]="$terminfo[kbs]"
+key[Delete]="$terminfo[kdch1]"
+key[Up]="$terminfo[kcuu1]"
+key[Down]="$terminfo[kcud1]"
+key[Left]="$terminfo[kcub1]"
+key[Right]="$terminfo[kcuf1]"
+key[PageUp]="$terminfo[kpp]"
+key[PageDown]="$terminfo[knp]"
+
+# setup key accordingly
+[[ -n "$key[Home]"      ]] && bindkey -- "$key[Home]"      beginning-of-line
+[[ -n "$key[End]"       ]] && bindkey -- "$key[End]"       end-of-line
+[[ -n "$key[Insert]"    ]] && bindkey -- "$key[Insert]"    overwrite-mode
+[[ -n "$key[Backspace]" ]] && bindkey -- "$key[Backspace]" backward-delete-char
+[[ -n "$key[Delete]"    ]] && bindkey -- "$key[Delete]"    delete-char
+[[ -n "$key[Up]"        ]] && bindkey -- "$key[Up]"        up-line-or-history
+[[ -n "$key[Down]"      ]] && bindkey -- "$key[Down]"      down-line-or-history
+[[ -n "$key[Left]"      ]] && bindkey -- "$key[Left]"      backward-char
+[[ -n "$key[Right]"     ]] && bindkey -- "$key[Right]"     forward-char
+
+# Finally, make sure the terminal is in application mode, when zle is
+# active. Only then are the values from $terminfo valid.
+if (( ${+terminfo[smkx]} )) && (( ${+terminfo[rmkx]} )); then
+    function zle-line-init () {
+        echoti smkx
+    }
+    function zle-line-finish () {
+        echoti rmkx
+    }
+    zle -N zle-line-init
+    zle -N zle-line-finish
+fi
+
 
 # Bind ctrl-left / ctrl-right
 bindkey "\e[1;5D" backward-word
-bindkey "\e[1;5C" forward-word"]]"
+bindkey "\e[1;5C" forward-word
 
 # Back to previous folder: Alt + LeftArrow
 # Up to parent folder: Alt + UpArrow
