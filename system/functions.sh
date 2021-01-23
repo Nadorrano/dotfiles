a555120c3b50b8674b3853e82f82bdff8bc8c786
Nadorrano <fonta.spezia@gmail.com>
2021-01-23 19:51:35 +0100

    Revert "Improve zsh and bash"
    This reverts commit aa6de8ce2ff93322b852129c3e0c9d49d4c45cfb.

--- system/functions.sh
+++ system/functions.sh
@@ -28,3 +28,17 @@ extract () {
 }
 
 
+# List all packages installed by date
+# add --desc to get the description of the package
+pacdate () {
+  if [[ "$1" == '--desc' ]]; then
+    awk '/%NAME%/{getline;PKG=$1} /%INSTALLDATE%/{getline
+    IDATE=strftime("%Y-%m-%d %H:%M",$1)} /%DESC%/{getline; DESC=$0
+    print IDATE" "PKG" -- "DESC}' /var/lib/pacman/local/*/desc|sort
+  else
+    awk '/%NAME%/{getline;PKG=$1} /%INSTALLDATE%/{getline
+    IDATE=strftime("%Y-%m-%d %H:%M",$1)
+    print IDATE" "PKG}' /var/lib/pacman/local/*/desc|sort
+  fi
+}
+
